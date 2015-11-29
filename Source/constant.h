bbfa041fd430ad572d6b19d3b3ea2cec69590429
hypoxiao <xiao_hb@qq.com>
2015-11-29 22:29:21 +0800

    delete duplicate files

--- Source/constant.h
+++
@@ -1,8 +0,0 @@
-//
-//  LayoutView.h
-//  CommentLaout
-//
-//  Created by zhongjunchi on 11/27/15.
-//  Copyright © 2015 launch10. All rights reserved.
-//
-#define  ColorName  [UIColor colorWithRed:66./255 green:133./255 blue:200./255 alpha:1]
